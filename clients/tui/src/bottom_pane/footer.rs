12c8fb40e5e03b81aad22153dd7f7a4b154f85a9
pak-ee <pak.ee.spbstu@gmail.com>
2026-05-09 14:59:51 +0300

    Move TUI footer helpers into cards

--- clients/tui/src/bottom_pane/footer.rs
+++ clients/tui/src/bottom_pane/footer.rs
@@ -1,6 +1,9 @@
 use ratatui::text::{Line, Span};
 
-use crate::visual::{VisualState, footer_plain_line, muted_style};
+use crate::{
+    cards::footer_plain_line,
+    visual::{VisualState, muted_style},
+};
 
 pub(crate) fn footer_line(state: &VisualState<'_>, width: usize) -> Line<'static> {
     Line::from(Span::styled(footer_plain_line(state, width), muted_style()))
